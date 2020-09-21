bc986a7fa4bf1eec0b2c56da08b4c40b0f36328a
hailong.chen <chenhailong@fr-inc.cn>
2020-09-21 20:17:07 +0800

    增加提醒

--- service-worker.js
+++ service-worker.js
@@ -19,3 +19,29 @@ registerRoute(
     ],
   })
 );
+
+// 安装阶段跳过等待，直接进入 active
+var cacheName = "cachev2"
+self.addEventListener('install', function (event) {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        Promise.all([
+            // 更新客户端
+            self.clients.claim(),
+
+            // 清理旧版本
+            caches.keys().then(function (cacheList) {
+                return Promise.all(
+                    cacheList.map(function (cacheName) {
+                        if (cacheName !== 'cachev1') {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
+    );
+});
